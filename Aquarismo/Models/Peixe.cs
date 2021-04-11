71ae6843723d6d5f7dae2596b8bef6c165fae4ea
Thyago <t.y_josef7@hotmail.com>
2021-04-11 18:40:23 -0300

    primeiro commit

---
+++ Aquarismo/Models/Peixe.cs
@@ -0,0 +1,29 @@
+﻿using Aquarismo.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aquarismo.Models
+{
+    public class Peixe
+    {
+        public Categoria Categoria { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Tipo_de_Agua { get; set; }
+
+        public string Familia { get; set; }
+
+        public string Especie { get; set; }
+
+        public string Região { get; set; }
+
+        public string Descriçao { get; set; }
+
+        
+
+        
+    }
+}
